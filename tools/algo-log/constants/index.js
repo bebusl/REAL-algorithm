a435895bd91fe068b44032a2681b459d44f6bbd0
이진희 <49019236+bebusl@users.noreply.github.com>
2026-05-06 23:52:36 +0900

    feat(algo): language 상수 추가

--- tools/algo-log/constants/index.js
+++ tools/algo-log/constants/index.js
@@ -1,3 +1,4 @@
 // src/consts/index.js
 export * from "./platforms.js";
 export * from "./categories.js";
+export * from "./languages.js";
---
+++ tools/algo-log/constants/languages.js
@@ -0,0 +1,13 @@
+export const LANGUAGE = {
+  JS: "js",
+  PY: "py",
+  JAVA: "java",
+  CPP: "cpp",
+};
+
+export const LANGUAGE_KR = {
+  [LANGUAGE.JS]: "JavaScript",
+  [LANGUAGE.PY]: "Python",
+  [LANGUAGE.JAVA]: "Java",
+  [LANGUAGE.CPP]: "C++",
+};
